932dcfaeea9e61c4eeb03a60975c7b35d7a593e5
JoshuaBentley <joshuabentley911@gmail.com>
2023-04-19 15:17:45 -0700

    added some styling

--- client/src/app.js
+++ client/src/app.js
@@ -2,14 +2,17 @@ import React from "react";
 import {Container, AppBar, Typography, Grow, Grid, } from '@material-ui/core'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
+import useStyles from './styles'
 import SWlogo from './img/starwarslogo.png'
 
 const App = () => {
+    const styling = useStyles()
+
     return (
         <Container maxidth="lg">
-            <AppBar position="static" color="inherit">
+            <AppBar className={styling.appBar} position="static" color="inherit">
                 <Typography variant="h2" align="center">Stars Fan Page</Typography>
-                <img src={SWlogo} alt="Star Wars logo" height="160"/>
+                <img className={styling.image} src={SWlogo} alt="Star Wars logo" height="60"/>
             </AppBar>
             <Grow in>
                 <Container>
