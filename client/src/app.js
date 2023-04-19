0a3e85351080f10d4a029b690d649406bde08e0d
JoshuaBentley <joshuabentley911@gmail.com>
2023-04-19 15:04:59 -0700

    set jsx for form and posts page and linked to app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,12 +1,29 @@
 import React from "react";
+import {Container, AppBar, Typography, Grow, Grid, } from '@material-ui/core'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import SWlogo from './img/starwarslogo.png'
 
 const App = () => {
     return (
-        <div>
-            <hi>
-                Hello Wisoncson.
-            </hi>
-        </div>
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Stars Fan Page</Typography>
+                <img src={SWlogo} alt="Star Wars logo" height="160"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} xm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} xm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
