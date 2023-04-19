7cbd34afde8fd3836bd0c8ba77fc01e5e7bdad96
JoshuaBentley <joshuabentley911@gmail.com>
2023-04-19 11:48:54 -0700

    set up server and first post router

---
+++ client/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <hi>
+                Hello Wisoncson.
+            </hi>
+        </div>
+    )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import postRoutes from './Routes/posts.js'
+
+const app = express() 
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+const connectToMongo = 'mongodb+srv://joshuabentley:traya1213@cluster0.urzgrho.mongodb.net/test'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(connectToMongo)
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((err) => console.log(err.message))
